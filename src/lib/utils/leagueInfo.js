a5e8a365a5b10a7a94336a888771806b3a275d17
TheRealJRad <42502544+TheRealJRad@users.noreply.github.com>
2024-11-27 14:21:11 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,7 +1,7 @@
 /*   STEP 1   */
 export const leagueID = "1048651172120317952"; // your league ID
 export const leagueName = "This Dynasty League Is Missing A; // your league name
-export const dues = 50; // (optional) used in template constitution page
+export const dues = 500; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
 export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VITE_CONTENTFUL_SPACE environment variables
 
